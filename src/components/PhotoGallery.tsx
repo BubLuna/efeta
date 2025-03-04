a5f1f363c9870f6ac4161b634a746b1ba414812b
Beca <rebecalunardibublitz@gmail.com>
2025-03-04 11:35:53 -0300

    Update PhotoGallery.tsx
    EFETA --> ÉFETA

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -57,7 +57,7 @@ const PhotoGallery = () => {
         <div className="text-center mb-12">
           <h2 className="text-3xl md:text-4xl font-bold mb-4 font-display text-gray-900">Galeria de Fotos</h2>
           <p className="text-lg text-gray-600 max-w-2xl mx-auto">
-            Confira alguns momentos especiais de edições anteriores do Acampamento EFETA.
+            Confira alguns momentos especiais de edições anteriores do Acampamento Éfeta.
           </p>
         </div>
         
