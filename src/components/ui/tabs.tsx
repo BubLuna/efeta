df8a7363c672f651dda3b2a5c1d1cc14bf170792
Beca <rebecalunardibublitz@gmail.com>
2025-03-12 14:35:37 +0000

    teste

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -27,7 +27,25 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      // Layout
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm",
+      
+      // Padding and Text
+      "px-3 py-1.5 text-sm font-medium",
+      
+      // Focus and Ring
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background",
+    
+      // Transition
+      "transition-all",
+    
+      // Disabled State
+      "disabled:pointer-events-none disabled:opacity-50",
+    
+      // Active State
+      "data-[state=active]:bg-background {background-color: rgb(249 115 22);} data-[state=active]:text-foreground {color: hsl(0deg 0% 100%);} data-[state=active]:shadow-sm",
+      
+      // Additional custom classes
       className
     )}
     {...props}
